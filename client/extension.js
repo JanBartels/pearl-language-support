30e82cb662c7915a2d2ed466cd1ff35f955781f6
Jan Bartels <j.bartels@arcor.de>
2026-01-14 23:43:13 +0100

    Jahreszahl auf 2026

--- client/extension.js
+++ client/extension.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Jan Bartels
+ * Copyright (C) 2025, 2026 Jan Bartels
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
